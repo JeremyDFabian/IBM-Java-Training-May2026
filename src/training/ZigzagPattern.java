a919778b8ded83d704258855dc86bbd854b2b36a
Jeremy D. Fabian <jeremyfabian@Jeremys-MacBook-Air.local>
2026-05-19 22:00:57 +0800

    Submit Day 2 Java Training Assignments

--- src/training/ZigzagPattern.java
+++ src/training/ZigzagPattern.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 public class ZigzagPattern {
     public static void main(String[] args) {
     	
-    	
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter grid size: ");
         int grid = sc.nextInt();
-  
-    
+     
         for (int i = 0; i < grid; i++) {	
             for (int j = 0; j < grid; j++){
        
@@ -25,9 +23,7 @@ public class ZigzagPattern {
             	System.out.print(num + " ");
             }
            System.out.println();
-           
-        	 
+            
         }
-        
     }
 }
